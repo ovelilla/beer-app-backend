ff5e42a2f37a73e3f3ab7e73b04d4b6051b26b85
Oscar <o.velillaa@gmail.com>
2024-04-06 02:18:25 +0200

    updates

--- routes/beersRoutes.js
+++ routes/beersRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
 import {
-    readAll,
+  create,
+  readAll,
+  remove,
+  removeMultiple,
+  update,
 } from "../controllers/beersController.js";
 import checkAuth from "../middleware/checkAuth.js";
 
 const router = express.Router();
 
 router.get("/beers", checkAuth, readAll);
+router.post("/beers", checkAuth, create);
+router.put("/beers/:id", checkAuth, update);
+router.delete("/beers/:id", checkAuth, remove);
+router.delete("/beers", checkAuth, removeMultiple);
 
 export default router;
